4d7c83a6d8b4fca1cf4418c9b34e0734c5cfcad7
miguel neph <miguel96ss@gmail.com>
2023-11-13 16:11:10 +0000

    initial commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+function multiplicar(x: number, y: number): number {
+    return x * y;
+}
+
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 3);
+const mensagemSaudacao = saudacao("Maria");
+
+console.log(resultadoMultiplicacao); 
+console.log(mensagemSaudacao);      
+
+
+
+
+
